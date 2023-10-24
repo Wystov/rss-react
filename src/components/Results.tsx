63d369f0476bb80b0e7c02ca67f0aa1d18ab36e8
Wystov <wystov@gmail.com>
2023-10-24 18:21:26 +0200

    feat: create two sections

---
+++ src/components/Results.tsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Results extends Component {
+  render() {
+    return (
+      <section>
+        <h1>Results</h1>
+      </section>
+    );
+  }
+}
+
+export default Results;
---
+++ src/components/Search.tsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <section>
+        <input type="text" />
+        <button>Search</button>
+      </section>
+    );
+  }
+}
+
+export default Search;

f4887727af460f5b84fe3a99720c009d10f30e03
Wystov <wystov@gmail.com>
2023-11-10 11:47:55 +0100

    feat: test for search component

---
+++ src/__tests__/Search.test.tsx
@@ -0,0 +1,47 @@
+import { BrowserRouter, RouterProvider } from 'react-router-dom';
+import Search from '../components/Search';
+import { SearchContext } from '../App';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { vi } from 'vitest';
+import { data } from './mock-data';
+import router from '../router';
+
+describe('Search component tests', () => {
+  it('clicking the Search button saves the entered value to the local storage', async () => {
+    const initialValue = '';
+
+    render(
+      <BrowserRouter>
+        <SearchContext.Provider value={initialValue}>
+          <Search isFetching={false} />
+        </SearchContext.Provider>
+      </BrowserRouter>
+    );
+
+    const input = screen.getByRole('textbox');
+    const submitButton = screen.getByRole('button');
+
+    expect((input as HTMLInputElement).value).toBe(initialValue);
+
+    await userEvent.type(input, 'value');
+    await userEvent.click(submitButton);
+
+    expect(localStorage.getItem('sw-search-query')).toBe('value');
+  });
+
+  it('Component retrieves the value from the local storage upon mounting', () => {
+    vi.doMock('../api/getData', () => ({
+      getData: vi.fn().mockResolvedValue(data),
+    }));
+
+    const valueFromLs = 'value from ls';
+    localStorage.setItem('sw-search-query', valueFromLs);
+
+    render(<RouterProvider router={router} />);
+
+    const input = screen.getByRole('textbox');
+
+    expect((input as HTMLInputElement).value).toBe(valueFromLs);
+  });
+});
